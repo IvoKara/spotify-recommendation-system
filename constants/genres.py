26a6fe17b181403611f7cf3b09b1a9e91f5b4e18
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-09 19:04:46 +0200

    feat: consider popular genres from everynoise.com

---
+++ constants/genres.py
@@ -0,0 +1,11 @@
+main_genres = "pop", "rap", "rock", "latin", "r&b", "edm"
+
+
+subgenres = (
+    ("dance pop", "post-teen pop", "talent show", "boy band"),
+    ("trap", "pop rap", "southern hip hop", "hip-hop"),
+    ("classic rock", "hard rock", "heartland rock", "permanent wave"),
+    ("urbano latino", "trap latino", "reggaeton", "latin hip hop"),
+    ("new jack swing", "neo soul", "contemporary r&b", "pop r&b"),
+    ("electro house", "complextro", "big room", "slap house"),
+)
