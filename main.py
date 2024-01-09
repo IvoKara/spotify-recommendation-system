81f93a666ce95f1d86121b826c1b7877606875af
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-10 00:44:32 +0200

    refactor: change the way data is handled

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
+from typing import List, Tuple
+
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 
-from typing import Set
-
-from constants.playlist import top_songs_id
 from utils.url import url_to_id
 
 spotify = Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-result = spotify.playlist(top_songs_id)
-
 
 if __name__ == "__main__":
-    track_ids: Set[str] = set([])
-    for i in 0, 50:
-        search = spotify.search("slap house", offset=i, limit=50, type="playlist")
+    # df = pd.DataFrame(columns=["track_id", "track_name", "artists", "genres"])
+    tracks_info: List[Tuple[str, str]] = []
+    genre = "slap house"
+    for i in 0, 50, 100:
+        print(i)
+        search = spotify.search(genre, offset=i, limit=50, type="playlist")
         playlists = search["playlists"]["items"]
-        for playlist in playlists:
-            print(f'{playlist["name"]} - {playlist["tracks"]["total"]} tracks total')
-            playlist_id = url_to_id(playlist["href"])
-            print(playlist_id)
 
-            playlist_data = spotify.playlist_tracks(
-                playlist_id, fields="items(track(id,name))"
+        for playlist in playlists:
+            # print(f'{playlist["name"]} - {playlist["tracks"]["total"]} tracks total')
+
+            playlist_id = url_to_id(playlist["href"])
+            # print(playlist_id)
+
+            playlist_tracks = spotify.playlist_tracks(
+                playlist_id, fields="items(track(id))"
+            )["items"]
+
+            tracks_info.extend(
+                [
+                    (item["track"]["id"], genre)
+                    for item in playlist_tracks
+                    if item["track"] is not None
+                ]
             )
 
-            items = filter(lambda x: x["track"] is not None, playlist_data["items"])
-            track_ids.update({item["track"]["id"] for item in items})
-
-    print(len(track_ids))
+    print(len(set(tracks_info)))
     # tracks_number = sum([playlist['tracks']['total'] for playlist in playlists])
     # print(tracks_number)
