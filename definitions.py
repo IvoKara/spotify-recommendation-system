480d74b0d36ea3b40161eb92c49eadaf3950c0e7
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-13 23:26:02 +0200

    refactor(definitions): change filename in path

--- definitions.py
+++ definitions.py
@@ -3,5 +3,5 @@ from os import path
 ROOT_DIR = path.dirname(path.abspath(__file__))  # This is Project Root
 LOCAL_DATA_DIR = path.join(ROOT_DIR, "data")
 
-TRACKS_PATH = path.join(LOCAL_DATA_DIR, "tracks.csv")
+TRACKS_PATH = path.join(LOCAL_DATA_DIR, "tracks_basis.csv")
 TRACKS_WITH_FEATURES_PATH = path.join(LOCAL_DATA_DIR, "tracks_with_features.csv")
