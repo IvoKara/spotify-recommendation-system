82233ff2aaf9ee598cb76536a860d3bca3e292f5
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-14 01:37:21 +0200

    fix(main): structure the data usage flow, demo

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from os import path
 
-from definitions import TRACKS_PATH
+from definitions import TRACKS_PATH, TRACKS_WITH_FEATURES_PATH
 from scripts.fetch_api_data import collect_general_track_data
 
-if __name__ == "__main__":
-    if not path.isfile(TRACKS_PATH):
-        collect_general_track_data()
 
-    print("successfully gained data from Spotify's API")
+def load_data():
+    if not path.isfile(TRACKS_WITH_FEATURES_PATH):
+        if not path.isfile(TRACKS_PATH):
+            print("No basic track date. Calling the API...")
+            collect_general_track_data()
+            print("Successfully gained data from Spotify's API")
+        else:
+            print("There is some local data here. Using it...")
+
+        print("Now adding audio features to the tracks...")
+        # include_features_to_data()
+
+    # prepare data
+
+    # return prepared data
+
+
+if __name__ == "__main__":
+    load_data()
+
     # df = pd.read_csv(TRACKS_PATH, delimiter=";")
     # df = prepare(df)
-
     # print(df.head())
