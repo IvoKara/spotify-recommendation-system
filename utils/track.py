c5932d02a42af99fd6bc1a90a2377793e3d69a74
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-13 22:53:19 +0200

    fix(tracks): no album image check

--- utils/track.py
+++ utils/track.py
@@ -1,9 +1,11 @@
 def prepare(track: dict) -> dict:
+    album_images = track["album"]["images"]
+
     prepared = {
         "track_id": track["id"],
         "track_name": track["name"],
         "artists": [artist["name"] for artist in track["artists"]],
-        "image_url": track["album"]["images"][0]["url"],
+        "image_url": album_images[0]["url"] if len(album_images) else None,
         "release_date": track["album"]["release_date"],
     }
 
