32f3125369b52cb10bde20f28a61cb00faafe62f
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-14 01:36:47 +0200

    feat: preprocessing functions for data

---
+++ scripts/preprocess_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def remove_duplicates(df: pd.DataFrame):
+    return df.drop_duplicates(subset=["track_id", "genres"])
+
+
+def merge_by_genre(df: pd.DataFrame):
+    return df.groupby("track_id")["genres"].apply(list).reset_index()
+
+
+def preprocess(df: pd.DataFrame):
+    df = remove_duplicates(df)
+    df = merge_by_genre(df)
+
+    return df
+
+
+# df = pd.read_csv(TRACKS_WITH_FEATURES_PATH, delimiter=";")
+# tracks_with_features_df = prepare(df)
