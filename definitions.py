3ddcd2aafd9ad5448f549f93add439618cbdad5b
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-13 21:40:07 +0200

    feat(definitions): add data paths

--- definitions.py
+++ definitions.py
@@ -2,3 +2,6 @@ from os import path
 
 ROOT_DIR = path.dirname(path.abspath(__file__))  # This is Project Root
 LOCAL_DATA_DIR = path.join(ROOT_DIR, "data")
+
+TRACKS_PATH = path.join(LOCAL_DATA_DIR, "tracks.csv")
+TRACKS_WITH_FEATURES_PATH = path.join(LOCAL_DATA_DIR, "tracks_with_features.csv")
