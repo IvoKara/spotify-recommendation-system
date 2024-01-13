e974ec801f30a7d842205abb03aebf8d3a02843c
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-01-14 01:38:02 +0200

    fix(preprocess): update merge to be as expected

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -6,12 +6,16 @@ def remove_duplicates(df: pd.DataFrame):
 
 
 def merge_by_genre(df: pd.DataFrame):
-    return df.groupby("track_id")["genres"].apply(list).reset_index()
+    return df.groupby("track_id").agg(tuple).map(list).reset_index()
 
 
 def preprocess(df: pd.DataFrame):
+    print("with duplicates ", len(df.index))
     df = remove_duplicates(df)
+    print("without duplicates ", len(df.index))
+
     df = merge_by_genre(df)
+    print("after merge", len(df.index))
 
     return df
 
